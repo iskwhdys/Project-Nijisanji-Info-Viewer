214092f90250012c880a8abaa5bcdfb13ddd966a
iskwhdys <iskwhdys@gmail.com>
2020-01-13 15:10:55 +0900

    プリレンダリング対応

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,19 @@
+var Path = require('path')
+var PrerenderSpaPlugin = require('prerender-spa-plugin')
+
 module.exports = {
   configureWebpack: {
     devtool: "source-map",
+    plugins: [
+      new PrerenderSpaPlugin(
+        // Absolute path to compiled SPA
+        Path.join(__dirname, 'html'),
+        // List of routes to prerender
+        [ '/', '/ranking', '/manager' ]
+      )
+    ]
   },
   
-  outputDir: 'docs',
-  assetsDir: './',
-  publicPath: './'
+  outputDir: 'html',
+  
 };
