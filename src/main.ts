92343d16faf4c6a5bfad02a47758a2da524ffa91
iskwhdys <iskwhdys@gmail.com>
2020-02-23 17:29:58 +0900

    GAをVueAnalyticsに変更

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './plugins/router'
-import store from './plugins/store'
 import vuetify from './plugins/vuetify'
+import VueAnalytics from 'vue-analytics';
 
 Vue.config.productionTip = false
 
@@ -11,3 +11,8 @@ new Vue({
   vuetify,
   render: h => h(App),
 }).$mount('#app')
+
+Vue.use(VueAnalytics, {
+  id: 'UA-156666381-1',
+  router,
+});
